0d4f3c2cc76af46f26e669459c57a1fd8a9fd6f6
Jana Slavnic <jana_slavnic@sluz.ch>
2026-03-19 08:15:53 +0100

    settings page versuch fehlerbehebung 500 sever error

--- src/routes/settings/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ locals }) => {
-	const session = await locals.safeGetSession();
-	return {
-		email: session?.user?.email ?? null
-	};
-};
