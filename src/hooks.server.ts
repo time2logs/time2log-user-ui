e74c2c8e650434c5466d06afc983cdf23672b739
Joen Arnet <joen_arnet@sluz.ch>
2026-04-02 13:19:19 +0200

    schema auf app geändert

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -40,7 +40,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		throw new Error('SUPABASE_SERVICE_ROLE_KEY is not configured');
 	}
 	event.locals.supabaseServiceRole = createClient(PUBLIC_SUPABASE_URL, serviceRoleKey, {
-		db: { schema: 'admin' }
+		db: { schema: 'app' }
 	});
 
 	event.locals.safeGetSession = async () => {
