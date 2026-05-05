3e6888cbe84400770b4c5cbfa8a2e5f1aa622cd1
jana <jana@slavnic.com>
2026-05-05 10:54:04 +0200

    Fehler behoben

--- src/lib/components/theme-toggle.svelte
+++ src/lib/components/theme-toggle.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-    import {theme} from '$lib/themeStore';
-    import {Sun, Moon} from '@lucide/svelte';
+    import { theme } from '$lib/themeStore';
+    import { Button } from '$lib/components/ui/button';
+    import { Sun, Moon } from 'lucide-svelte';
 </script>
 
-<button onclick={() => theme.toggle()} class="...">
+<Button variant="ghost" size="icon" onclick={() => theme.toggle()}>
     {#if $theme === 'dark'}
-        <Sun size={18}/>
+        <Sun size={18} />
     {:else}
-        <Moon size={18}/>
+        <Moon size={18} />
     {/if}
-</button>
+</Button>
