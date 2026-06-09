733555e8334605bfe63300fb11bd31850514dd4c
ellodev <ello.developers@gmail.com>
2026-06-09 08:42:45 +0200

    pwa first starting point

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,5 +14,7 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head
+	><link rel="icon" href={favicon} /> <link rel="manifest" href="/manifest.json" /></svelte:head
+>
 {@render children()}
